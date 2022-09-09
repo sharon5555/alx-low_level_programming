35975dd9f1da91272d4fa2e6a08813db98bfbd5e
Naza <sharon.doris54@gmail.com>
2022-09-09 03:08:41 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h> 
- /** 
-  *   * main - entry point 
-  *    * Return: 0 
-  *     */ 
- int main(void)
-{ 
-	   puts("\"Programming is like building a multilingual puzzle"); 
-	   return (0); 
+#include <stdio.h>
+/**
+  * main - Entry point
+  *
+  * Return: always 0 (Success)
+  */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

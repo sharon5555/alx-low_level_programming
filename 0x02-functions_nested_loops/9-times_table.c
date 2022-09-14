dc20690ef73d13157a71acbc5ec34819b4820ea3
sharon5555 <sharon.doris54@gmail.com>
2022-09-13 21:46:34 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * times_table - a function that prints the 9 time table starting with 0
- * rone = row cone = column d = digits of current result
+ * rone = row, cone = column, d = digits of current result
  * Return: times table
  * add extra space past single digit
  */
